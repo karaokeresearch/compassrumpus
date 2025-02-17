40d7e482c5c785df5d240a5d17371a5cc1ff3568
Ross Brackett <krc@brassrocket.com>
2025-02-16 20:56:47 -0500

    file system re-org

--- north.js
+++ north.js
@@ -215,7 +215,7 @@ let sound=[];
 
 
 function customLoadBuffer(context, url, callback) {
-  fetch(url)
+  fetch("media/" + url)
     .then(response => {
       if (!response.ok) {
         throw new Error('Network response was not ok: ' + response.statusText);
